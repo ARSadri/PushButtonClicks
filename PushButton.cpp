ac2d0e6a71ba456d23c07558603bceea44da7e5f
ARSadri <51805937+ARSadri@users.noreply.github.com>
2019-06-14 15:23:42 +1000

    Update PushButton.cpp

--- PushButton.cpp
+++ PushButton.cpp
@@ -6,7 +6,7 @@
  */
 
 #include "PushButton.h"
-
+//The constructor
 PushButton::PushButton(unsigned long inDeadDuration, unsigned long inlongDurationThreshold, unsigned long indebounceThreshold, bool inpressed) {
 	DeadDuration = inDeadDuration;
 	longDurationThreshold = inlongDurationThreshold;
@@ -19,11 +19,18 @@ PushButton::PushButton(unsigned long inDeadDuration, unsigned long inlongDuratio
 	shortPressedCnt = 2;
 }
 
-
+/* Long Press/hold/release is supported.
+If the button is pressed a counter counts how much time it has been in pressed state.
+if while it is held active, the duration passes a threshold, it is a long_press
+if released and the duration counter is higher than that threshold, its a long_release
+If it is not a long release and still the duration was higher than debouncing threshold, its a click.
+if another click does not arrive within the duration of bein dead, it was a click, 
+if it came and it was a short one its a double click and so on
+*/
 unsigned char PushButton::buttonCheck(const unsigned int current_millies, bool currentPinStatus) {
 	unsigned char output = 0;
 	if (currentPinStatus==pressed) {
-			pressedDuratoin += current_millies - lastCheckedTime;
+		pressedDuratoin += current_millies - lastCheckedTime;
 		if (pressedDuratoin > longDurationThreshold && longPressflag == 0) {
 			output = 1;	//long Event without release
 			longPressflag = 1;
